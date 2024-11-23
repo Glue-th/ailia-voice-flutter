b06d54576ba1997b8d9631fc71a12761aab00e98
t-pong <phongtran030490@gmail.com>
2024-11-23 23:49:55 +0700

    update code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:ailia/ailia_license.dart';
+import 'package:flutter/services.dart';
+
+import 'package:share_plus/share_plus.dart';
 
 import 'utils/download_model.dart';
 import 'text_to_speech.dart';
@@ -19,22 +22,63 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final navigatorKey = GlobalKey<NavigatorState>();
+
   String _predictText = 'Model Downloading...';
+
+  // String _text = "おはようございます。こんにちは。今日はお元気ですか？";
+  String _text = "This is an album";
+  var outputPath;
   final _textToSpeech = TextToSpeech();
+
+  // Prepare model file
+  String encoderFile = "";
+  String decoderFile = "";
+  String postnetFile = "";
+  String waveglowFile = "";
+  String? sslFile;
+
+  String? dicFolderOpenJtalk;
+  String? dicFolderG2PEn;
+
+  int _downloadCnt = 0;
+  List<String> modelList = List<String>.empty(growable: true);
+
+  // int modelType = TextToSpeech.MODEL_TYPE_TACOTRON2;
+  // int modelType = TextToSpeech.MODEL_TYPE_GPT_SOVITS_JA;
+
+  int modelType = TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN;
+
+  // voice
+  var voices = List<String>.empty(growable: true);
+  var voiceValue = "";
+
+  // models
+  var models = List<String>.empty(growable: true);
+  var modelValue = "";
 
   @override
   void initState() {
     super.initState();
 
     _ailiaVoiceDownloadModel();
+
+    // init voices
+    voices = _textToSpeech.getVoicesList(modelType);
+    voiceValue = voices.first;
+
+    models = _textToSpeech.getModels();
+    modelValue = models.first;
   }
 
-  int _downloadCnt = 0;
-  List<String> modelList = List<String>.empty(growable: true);
+  @override
+  void dispose() {
+    _textToSpeech.destroy();
+    super.dispose();
+  }
 
-  //int modelType = TextToSpeech.MODEL_TYPE_TACOTRON2;
-  int modelType = TextToSpeech.MODEL_TYPE_GPT_SOVITS_JA;
-  //int modelType = TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN;
+  Future<String> loadString() async =>
+      await rootBundle.loadString('assets/voices.txt');
 
   void _ailiaVoiceDownloadModel() {
     modelList = _textToSpeech.getModelList(modelType);
@@ -44,6 +88,9 @@ class _MyAppState extends State<MyApp> {
   void _ailiaVoiceDownloadModelOne() {
     String url =
         "https://storage.googleapis.com/ailia-models/${modelList[_downloadCnt + 0]}/${modelList[_downloadCnt + 1]}";
+    setState(() {
+      _predictText = "downloading: $url";
+    });
     downloadModel(url, modelList[_downloadCnt + 1], (file) {
       _downloadCnt = _downloadCnt + 2;
       if (_downloadCnt >= modelList.length) {
@@ -58,57 +105,254 @@ class _MyAppState extends State<MyApp> {
     // Check and download ailia SDK license
     await AiliaLicense.checkAndDownloadLicense();
 
-    // Prepare model file
-    String encoderFile = "";
-    String decoderFile = "";
-    String postnetFile = "";
-    String waveglowFile = "";
-    String? sslFile;
-
-    if (modelType == TextToSpeech.MODEL_TYPE_TACOTRON2){
+    if (modelType == TextToSpeech.MODEL_TYPE_TACOTRON2) {
       encoderFile = await getModelPath("encoder.onnx");
       decoderFile = await getModelPath("decoder_iter.onnx");
       postnetFile = await getModelPath("postnet.onnx");
       waveglowFile = await getModelPath("waveglow.onnx");
     }
 
-    if (modelType == TextToSpeech.MODEL_TYPE_GPT_SOVITS_JA || modelType == TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN) {
+    if (modelType == TextToSpeech.MODEL_TYPE_GPT_SOVITS_JA ||
+        modelType == TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN) {
       encoderFile = await getModelPath("t2s_encoder.onnx");
       decoderFile = await getModelPath("t2s_fsdec.onnx");
       postnetFile = await getModelPath("t2s_sdec.opt.onnx");
       waveglowFile = await getModelPath("vits.onnx");
       sslFile = await getModelPath("cnhubert.onnx");
-    }
-
-    String? dicFolderOpenJtalk;
-    String? dicFolderG2PEn;
-    if (modelType == TextToSpeech.MODEL_TYPE_GPT_SOVITS_JA || modelType == TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN) {
       dicFolderOpenJtalk = await getModelPath("open_jtalk_dic_utf_8-1.11/");
     }
-    if (modelType == TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN){
+
+    if (modelType == TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN) {
       dicFolderG2PEn = await getModelPath("/");
     }
 
-    String targetText = "Hello world.";
-    String outputPath = await getModelPath("temp.wav");
+    var duration = await _textToSpeech.openModel(
+        encoderFile,
+        decoderFile,
+        postnetFile,
+        waveglowFile,
+        sslFile,
+        dicFolderOpenJtalk,
+        dicFolderG2PEn,
+        modelType);
 
-    await _textToSpeech.inference(targetText, outputPath, encoderFile,
-        decoderFile, postnetFile, waveglowFile, sslFile, dicFolderOpenJtalk, dicFolderG2PEn, modelType);
+    // String targetText = "Hello world.";
+    // String outputPath = await getModelPath("temp.wav");
+    // await _textToSpeech.inference(targetText, outputPath);
 
     setState(() {
-      _predictText = "finish";
+      _predictText = "Model $modelType Loaded ($duration ms)";
     });
+  }
+
+  void tts(String targetText) async {
+    try {
+      outputPath = await getModelPath("t_$targetText.wav");
+      var duration = await _textToSpeech.inference(targetText, outputPath);
+      setState(() {
+        _predictText = "Process ($duration ms):\n$outputPath";
+      });
+    } catch (e) {
+      print(e);
+      setState(() {
+        _predictText = "Inference $targetText error: $e";
+      });
+    }
+  }
+
+  void _showDialog(String title, String content) {
+    final context = navigatorKey.currentState!.overlay!.context;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: SingleChildScrollView(
+            child: Text(content),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('ailia Voice Sample'),
         ),
-        body: Center(
-          child: Text('Running on: $_predictText\n'),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text("Model"),
+                  const SizedBox(width: 16.0),
+                  DropdownMenu<String>(
+                    initialSelection: modelValue,
+                    onSelected: (String? value) async {
+                      // This is called when the user selects an item.
+                      try {
+                        modelValue = value!;
+
+                        if (modelValue == "GPT_SOVITS_EN") {
+                          modelType = TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN;
+                          dicFolderG2PEn = await getModelPath("/");
+                        }
+
+                        if (modelValue == "GPT_SOVITS_JA") {
+                          modelType = TextToSpeech.MODEL_TYPE_GPT_SOVITS_JA;
+                          dicFolderG2PEn = null;
+                        }
+
+                        var duration = await _textToSpeech.openModel(
+                            encoderFile,
+                            decoderFile,
+                            postnetFile,
+                            waveglowFile,
+                            sslFile,
+                            dicFolderOpenJtalk,
+                            dicFolderG2PEn,
+                            modelType);
+
+                        voices = _textToSpeech.getVoicesList(modelType);
+
+                        setState(() {
+                          voiceValue = voices.first;
+                          _predictText =
+                              "Load language $modelValue takes $duration ms";
+                        });
+                      } catch (e) {
+                        print(e);
+                        setState(() {
+                          _predictText = "Load language $modelValue error: $e";
+                        });
+                      }
+                    },
+                    dropdownMenuEntries:
+                        models.map<DropdownMenuEntry<String>>((String value) {
+                      return DropdownMenuEntry<String>(
+                          value: value, label: value);
+                    }).toList(),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text("Voices"),
+                  const SizedBox(width: 16.0),
+                  DropdownMenu<String>(
+                    initialSelection: voiceValue,
+                    onSelected: (String? value) async {
+                      // This is called when the user selects an item.
+                      try {
+                        voiceValue = value!;
+                        var totalTime =
+                            await _textToSpeech.loadVoice(voiceValue);
+                        setState(() {
+                          _predictText =
+                              "Load voice $voiceValue takes $totalTime ms";
+                        });
+                      } catch (e) {
+                        print(e);
+                        setState(() {
+                          _predictText = "Load voice $voiceValue error: $e";
+
+                          if (modelType ==
+                              TextToSpeech.MODEL_TYPE_GPT_SOVITS_EN) {
+                            _predictText =
+                                "$_predictText\nPLEASE TRY MODEL: GPT_SOVITS_JP";
+                            _showDialog(
+                                "Error", "Please try with model: GPT_SOVITS_JP");
+                          }
+                        });
+                      }
+                    },
+                    dropdownMenuEntries:
+                        voices.map<DropdownMenuEntry<String>>((String value) {
+                      return DropdownMenuEntry<String>(
+                          value: value, label: value);
+                    }).toList(),
+                  ),
+                  TextButton(
+                      onPressed: () async {
+                        String voiceListDesc =
+                            await rootBundle.loadString('assets/voices.txt');
+                        print(voiceListDesc);
+                        _showDialog("Voices", voiceListDesc);
+                      },
+                      child: const Text("ℹ️"))
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: TextEditingController()..text = _text,
+                onChanged: (text) {
+                  // Update your state with the input text here
+                  _text = text;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter text',
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your button press logic here
+                      setState(() {
+                        _predictText = "processing $_text";
+                      });
+
+                      tts(_text);
+                    },
+                    child: const Text('Play'),
+                  ),
+                  const SizedBox(width: 16.0),
+                  ElevatedButton(
+                    onPressed: () async {
+                      // Add your button press logic here
+                      _textToSpeech.play(outputPath);
+                    },
+                    child: const Text('Replay'),
+                  ),
+                  const SizedBox(width: 16.0),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final result = await Share.shareXFiles(
+                          [XFile(outputPath)],
+                          text: _text);
+
+                      if (result.status == ShareResultStatus.success) {
+                        print('Thank you for sharing the picture!');
+                      }
+                    },
+                    child: const Text('Share'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              Text('$_predictText\n'),
+            ],
+          ),
         ),
       ),
     );
--- example/lib/text_to_speech.dart
+++ example/lib/text_to_speech.dart
@@ -24,20 +24,87 @@ class Speaker {
     final player = AudioPlayer();
     await player.play(DeviceFileSource(outputPath));
   }
+
+  void playOnly(String outputPath) async {
+    final player = AudioPlayer();
+    await player.play(DeviceFileSource(outputPath));
+  }
 }
 
 class TextToSpeech {
   final _speaker = Speaker();
-  final _ailiaVoiceModel = AiliaVoiceModel();
+  AiliaVoiceModel? _ailiaVoiceModel = AiliaVoiceModel();
 
   static const int MODEL_TYPE_TACOTRON2 = 0;
   static const int MODEL_TYPE_GPT_SOVITS_JA = 1;
   static const int MODEL_TYPE_GPT_SOVITS_EN = 2;
 
+  int modelType = -1;
+
+  init() {}
+
+  destroy() {
+    // Terminate
+    _ailiaVoiceModel?.close();
+    _ailiaVoiceModel = null;
+  }
+
+  List<String> getVoicesList(int modelType) {
+    List<String> voiceList = List<String>.empty(growable: true);
+
+    voiceList.add("taylor swift (2s)");
+    voiceList.add("taylor swift (4s)");
+    voiceList.add("taylor swift (15s)");
+    voiceList.add("trump (3s)");
+    voiceList.add("trump (9s)");
+    voiceList.add("ken");
+    voiceList.add("japan girl");
+    voiceList.add("ano (1)");
+    voiceList.add("ano (2)");
+    voiceList.add("boy11 (1)");
+    voiceList.add("boy11 (2)");
+    voiceList.add("ega (1)");
+    voiceList.add("ega (2)");
+    voiceList.add("hotta (1)");
+    voiceList.add("hotta (2)");
+    voiceList.add("kaz (1)");
+    voiceList.add("kaz (2)");
+    voiceList.add("man17 (1)");
+    voiceList.add("man17 (2)");
+    voiceList.add("man18 (1)");
+    voiceList.add("man18 (2)");
+    voiceList.add("sana (1)");
+    voiceList.add("sana (2)");
+    voiceList.add("woman1 (1)");
+    voiceList.add("woman1 (2)");
+    voiceList.add("woman2 (1)");
+    voiceList.add("woman2 (2)");
+    voiceList.add("woman3 (1)");
+    voiceList.add("woman3 (2)");
+    voiceList.add("woman10 (1)");
+    voiceList.add("woman10 (2)");
+    voiceList.add("woman15");
+    voiceList.add("woman17 (1)");
+    voiceList.add("woman17 (2)");
+    voiceList.add("woman23");
+
+    return voiceList;
+  }
+
+  List<String> getModels() {
+    List<String> modelList = List<String>.empty(growable: true);
+
+    modelList.add("GPT_SOVITS_EN");
+    modelList.add("GPT_SOVITS_JA");
+
+    return modelList;
+  }
+
   List<String> getModelList(int modelType) {
     List<String> modelList = List<String>.empty(growable: true);
 
-    if (modelType == MODEL_TYPE_GPT_SOVITS_JA || modelType == MODEL_TYPE_GPT_SOVITS_EN){
+    if (modelType == MODEL_TYPE_GPT_SOVITS_JA ||
+        modelType == MODEL_TYPE_GPT_SOVITS_EN) {
       modelList.add("open_jtalk");
       modelList.add("open_jtalk_dic_utf_8-1.11/char.bin");
 
@@ -66,7 +133,7 @@ class TextToSpeech {
       modelList.add("open_jtalk_dic_utf_8-1.11/unk.dic");
     }
 
-    if (modelType == MODEL_TYPE_GPT_SOVITS_EN){
+    if (modelType == MODEL_TYPE_GPT_SOVITS_EN) {
       modelList.add("g2p_en");
       modelList.add("averaged_perceptron_tagger_classes.txt");
 
@@ -89,7 +156,8 @@ class TextToSpeech {
       modelList.add("homographs.en");
     }
 
-    if (modelType == ailia_voice_dart.AILIA_VOICE_MODEL_TYPE_TACOTRON2) {
+    if (modelType == ailia_voice_dart.AILIA_VOICE_MODEL_TYPE_TACOTRON2 ||
+        modelType == MODEL_TYPE_TACOTRON2) {
       modelList.add("tacotron2");
       modelList.add("encoder.onnx");
 
@@ -103,7 +171,8 @@ class TextToSpeech {
       modelList.add("waveglow.onnx");
     }
 
-    if (modelType == ailia_voice_dart.AILIA_VOICE_MODEL_TYPE_GPT_SOVITS) {
+    if (modelType == ailia_voice_dart.AILIA_VOICE_MODEL_TYPE_GPT_SOVITS ||
+        modelType == MODEL_TYPE_GPT_SOVITS_EN) {
       modelList.add("gpt-sovits");
       modelList.add("t2s_encoder.onnx");
 
@@ -123,9 +192,7 @@ class TextToSpeech {
     return modelList;
   }
 
-  Future<void> inference(
-      String targetText,
-      String outputPath,
+  Future<int> openModel(
       String encoderFile,
       String decoderFile,
       String postnetFile,
@@ -135,53 +202,269 @@ class TextToSpeech {
       String? dicFolderG2PEn,
       int modelType) async {
     // Open and Inference
-    _ailiaVoiceModel.openModel(
+    var startTime = DateTime.now().millisecondsSinceEpoch;
+    this.modelType = modelType;
+    _ailiaVoiceModel!.openModel(
         encoderFile,
         decoderFile,
         postnetFile,
         waveglowFile,
         sslFile,
-        (modelType == MODEL_TYPE_TACOTRON2) ? ailia_voice_dart.AILIA_VOICE_MODEL_TYPE_TACOTRON2:ailia_voice_dart.AILIA_VOICE_MODEL_TYPE_GPT_SOVITS,
+        (modelType == MODEL_TYPE_TACOTRON2)
+            ? ailia_voice_dart.AILIA_VOICE_MODEL_TYPE_TACOTRON2
+            : ailia_voice_dart.AILIA_VOICE_MODEL_TYPE_GPT_SOVITS,
         ailia_voice_dart.AILIA_VOICE_CLEANER_TYPE_BASIC,
         ailia_voice_dart.AILIA_ENVIRONMENT_ID_AUTO);
-    if (dicFolderOpenJtalk != null){
-      _ailiaVoiceModel.openDictionary(dicFolderOpenJtalk, ailia_voice_dart.AILIA_VOICE_DICTIONARY_TYPE_OPEN_JTALK);
+    if (dicFolderOpenJtalk != null) {
+      _ailiaVoiceModel!.openDictionary(dicFolderOpenJtalk,
+          ailia_voice_dart.AILIA_VOICE_DICTIONARY_TYPE_OPEN_JTALK);
     }
-    if (dicFolderG2PEn != null){
-      _ailiaVoiceModel.openDictionary(dicFolderG2PEn, ailia_voice_dart.AILIA_VOICE_DICTIONARY_TYPE_G2P_EN);
+    if (dicFolderG2PEn != null) {
+      _ailiaVoiceModel!.openDictionary(
+          dicFolderG2PEn, ailia_voice_dart.AILIA_VOICE_DICTIONARY_TYPE_G2P_EN);
     }
-    if (modelType == MODEL_TYPE_GPT_SOVITS_JA || modelType == MODEL_TYPE_GPT_SOVITS_EN) {
-      ByteData data = await rootBundle.load("assets/reference_audio_girl.wav");
+
+    await this.loadVoice(getVoicesList(this.modelType).first);
+
+    var totalTime = DateTime.now().millisecondsSinceEpoch - startTime;
+    print("Load model take $totalTime ms");
+    print(
+        "If error code != 0, see here: https://github.com/axinc-ai/ailia-voice-flutter/blob/4f7f3866bccaccba45b81d3005795f078276a230/lib/ailia_voice.dart");
+    return totalTime;
+  }
+
+  Future<int> loadVoice(String voice) async {
+    var startTime = DateTime.now().millisecondsSinceEpoch;
+    print("Loading voice $voice");
+    if (modelType == MODEL_TYPE_GPT_SOVITS_JA ||
+        modelType == MODEL_TYPE_GPT_SOVITS_EN) {
+      // Japan girl
+      var referenceText = "水をマレーシアから買わなくてはならない。";
+      var referenceFile = "assets/reference_audio_girl.wav";
+
+      if (voice == "ken") {
+        referenceText = "Inference This is an album";
+        referenceFile = "assets/ken.wav";
+      }
+
+      if (voice == "taylor swift (2s)") {
+        referenceText = "This is a pretty dark album";
+        referenceFile = "assets/taylor_2s.wav";
+      }
+
+      if (voice == "taylor swift (4s)") {
+        referenceText = "If I had a really joyful experience making something. This is a pretty dark album";
+        referenceFile = "assets/taylor_4s.wav";
+      }
+
+      if (voice == "taylor swift (15s)") {
+        referenceText = "is just as important as how proud you are of it in the end. And I think it really informs how proud I am of something. If I had a really joyful experience making something. This is a pretty dark album, but I'd say I had more fun making it than any album I I.";
+        referenceFile = "assets/taylor_15s.wav";
+      }
+
+      if (voice == "trump (9s)") {
+        referenceText = "26 people that said that never happened. Who would say it? Who would say it? Nobody would say it. Stupid person or a person that's mentally";
+        referenceFile = "assets/trump_9s.wav";
+      }
+
+      if (voice == "trump (3s)") {
+        referenceText = "26 people that said that never happened.";
+        referenceFile = "assets/trump_3s.wav";
+      }
+
+      if (voice == "japan girl") {
+        referenceText = "水をマレーシアから買わなくてはならない。";
+        referenceFile = "assets/reference_audio_girl.wav";
+      }
+
+      if (voice == "ano (1)") {
+        referenceText = "ました、あのです。よろしゅうおねがいいたします";
+        referenceFile = "assets/ano.wav";
+      }
+
+      if (voice == "ano (2)") {
+        referenceText = "ました、あのです。よろしゅうお願い致します";
+        referenceFile = "assets/ano.wav";
+      }
+
+      if (voice == "boy11 (1)") {
+        referenceText = "ま、よろしくね！";
+        referenceFile = "assets/boy11.wav";
+      }
+
+      if (voice == "boy11 (2)") {
+        referenceText = "ま、宜しくね！";
+        referenceFile = "assets/boy11.wav";
+      }
+
+      if (voice == "ega (1)") {
+        referenceText = "この、どうがをとってる、きょう";
+        referenceFile = "assets/ega.wav";
+      }
+
+      if (voice == "ega (2)") {
+        referenceText = "この、動画をとってる、今日";
+        referenceFile = "assets/ega.wav";
+      }
+
+      if (voice == "hotta (1)") {
+        referenceText = "だいじょうぶだけどこれちょっと、じかんが";
+        referenceFile = "assets/hotta.wav";
+      }
+
+      if (voice == "hotta (2)") {
+        referenceText = "大丈夫だけどこれちょっと、時間が";
+        referenceFile = "assets/hotta.wav";
+      }
+
+      if (voice == "kaz (1)") {
+        referenceText = "じゃあここは、はいじょするでいいですよね";
+        referenceFile = "assets/kaz.wav";
+      }
+
+      if (voice == "kaz (2)") {
+        referenceText = "じゃあここは、排除するでいいですよね";
+        referenceFile = "assets/kaz.wav";
+      }
+
+      if (voice == "man17 (1)") {
+        referenceText = "よろしくたのむ";
+        referenceFile = "assets/man17.wav";
+      }
+
+      if (voice == "man17 (2)") {
+        referenceText = "宜しく頼む";
+        referenceFile = "assets/man17.wav";
+      }
+
+      if (voice == "man18 (1)") {
+        referenceText = "よろしくおねがいします";
+        referenceFile = "assets/man18.wav";
+      }
+
+      if (voice == "man18 (2)") {
+        referenceText = "宜しくお願いします";
+        referenceFile = "assets/man18.wav";
+      }
+
+      if (voice == "sana (1)") {
+        referenceText = "はるということで、ぜんしょく、ぜひつかって";
+        referenceFile = "assets/sana.wav";
+      }
+
+      if (voice == "sana (2)") {
+        referenceText = "春ということで、全色、是非使って";
+        referenceFile = "assets/sana.wav";
+      }
+
+      if (voice == "woman1 (1)") {
+        referenceText = "がんばったね";
+        referenceFile = "assets/woman1.wav";
+      }
+
+      if (voice == "woman1 (2)") {
+        referenceText = "頑張ったね";
+        referenceFile = "assets/woman1.wav";
+      }
+
+      if (voice == "woman2 (1)") {
+        referenceText = "じゅんびは、いいですか";
+        referenceFile = "assets/woman2.wav";
+      }
+
+      if (voice == "woman2 (2)") {
+        referenceText = "準備は、いいですか";
+        referenceFile = "assets/woman2.wav";
+      }
+
+      if (voice == "woman3 (1)") {
+        referenceText = "ごちそうさまでした";
+        referenceFile = "assets/woman3.wav";
+      }
+
+      if (voice == "woman3 (2)") {
+        referenceText = "ごちそう様でした";
+        referenceFile = "assets/woman3.wav";
+      }
+
+      if (voice == "woman10 (1)") {
+        referenceText = "よろしくたのむぞ";
+        referenceFile = "assets/woman10.wav";
+      }
+
+      if (voice == "woman10 (2)") {
+        referenceText = "宜しく頼むぞ";
+        referenceFile = "assets/woman10.wav";
+      }
+
+      if (voice == "woman15") {
+        referenceText = "やりました！";
+        referenceFile = "assets/woman15.wav";
+      }
+
+      if (voice == "woman17 (1)") {
+        referenceText = "よろしくおねがいね";
+        referenceFile = "assets/woman17.wav";
+      }
+
+      if (voice == "woman17 (2)") {
+        referenceText = "宜しくお願いね";
+        referenceFile = "assets/woman17.wav";
+      }
+
+      if (voice == "woman23") {
+        referenceText = "これでどう？";
+        referenceFile = "assets/woman23.wav";
+      }
+
+      print("Reference $referenceFile");
+
+      ByteData data = await rootBundle.load(referenceFile);
       final wav = Wav.read(data.buffer.asUint8List());
 
       List<double> pcm = List<double>.empty(growable: true);
 
       for (int i = 0; i < wav.channels[0].length; ++i) {
+        // print("add channel $i");
         for (int j = 0; j < wav.channels.length; ++j) {
           pcm.add(wav.channels[j][i]);
         }
       }
 
-      String referenceFeature = _ailiaVoiceModel.g2p("水をマレーシアから買わなくてはならない。",
-          ailia_voice_dart.AILIA_VOICE_G2P_TYPE_GPT_SOVITS_JA);
-      _ailiaVoiceModel.setReference(
+      String referenceFeature = _ailiaVoiceModel!.g2p(
+          referenceText,
+          modelType == MODEL_TYPE_GPT_SOVITS_JA
+              ? ailia_voice_dart.AILIA_VOICE_G2P_TYPE_GPT_SOVITS_JA
+              : ailia_voice_dart.AILIA_VOICE_G2P_TYPE_GPT_SOVITS_EN);
+      _ailiaVoiceModel!.setReference(
           pcm, wav.samplesPerSecond, wav.channels.length, referenceFeature);
     }
+    var totalTime = DateTime.now().millisecondsSinceEpoch - startTime;
+    print("Load voice $voice takes $totalTime ms");
+    return totalTime;
+  }
 
+  Future<int> inference(String targetText, String outputPath) async {
     // Get Audio and Play
+    var startTime = DateTime.now().millisecondsSinceEpoch;
     String targetFeature = targetText;
-    if (modelType == MODEL_TYPE_GPT_SOVITS_JA){
-      targetFeature = _ailiaVoiceModel.g2p(targetText,
-          ailia_voice_dart.AILIA_VOICE_G2P_TYPE_GPT_SOVITS_JA);
+    if (modelType == MODEL_TYPE_GPT_SOVITS_JA) {
+      targetFeature = _ailiaVoiceModel!
+          .g2p(targetText, ailia_voice_dart.AILIA_VOICE_G2P_TYPE_GPT_SOVITS_JA);
     }
-    if (modelType == MODEL_TYPE_GPT_SOVITS_EN){
-      targetFeature = _ailiaVoiceModel.g2p(targetText,
-          ailia_voice_dart.AILIA_VOICE_G2P_TYPE_GPT_SOVITS_EN);
+    if (modelType == MODEL_TYPE_GPT_SOVITS_EN) {
+      targetFeature = _ailiaVoiceModel!
+          .g2p(targetText, ailia_voice_dart.AILIA_VOICE_G2P_TYPE_GPT_SOVITS_EN);
     }
-    final audio = _ailiaVoiceModel.inference(targetFeature);
-    _speaker.play(audio, outputPath);
 
-    // Terminate
-    _ailiaVoiceModel.close();
+    final audio = _ailiaVoiceModel!.inference(targetFeature);
+    _speaker.play(audio, outputPath);
+    var totalTime = DateTime.now().millisecondsSinceEpoch - startTime;
+    print("Inference $targetText takes $totalTime ms");
+    return totalTime;
+  }
+
+  void play(String outputPath) {
+    _speaker.playOnly(outputPath);
   }
 }
